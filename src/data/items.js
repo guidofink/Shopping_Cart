4f71526a2b20de5f4d0ad8228042433aa77f4919
guidofink <guidofink01@hotmail.com>
2021-08-09 23:53:27 -0300

    1st commit

---
+++ src/data/items.js
@@ -0,0 +1,60 @@
+const items = [
+  {
+    id: 1,
+    name: "Botas",
+    price: 12.0,
+    category: "Footwear",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec nisl sagittis, iaculis metus non, bibendum urna. Ut urna enim, luctus sed pharetra eu, imperdiet varius mi. Ut venenatis neque leo, eu mattis tellus commodo eu.",
+  },
+  {
+    id: 2,
+    name: "Zapatillas",
+    price: 224.0,
+    category: "Footwear",
+    description:
+      "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In vitae blandit ex, interdum pretium lacus. Cras tincidunt tortor a quam lobortis, in fringilla ipsum volutpat. Nullam elementum consequat lorem non hendrerit. Curabitur porttitor accumsan ex.",
+  },
+  {
+    id: 3,
+    name: "Campera",
+    price: 132.0,
+    category: "Shirts",
+    description:
+      "Donec convallis lorem non nulla iaculis, vitae ultricies urna accumsan. Phasellus a cursus urna, et tempor mi. Fusce et semper sapien. Integer sollicitudin, tellus id volutpat efficitur, ligula tellus vehicula lorem, eget faucibus nunc metus suscipit ipsum. Duis quis pharetra eros, vitae vehicula augue. Nunc elementum diam ac felis iaculis, sit amet imperdiet velit elementum. Pellentesque elit ex, egestas nec nisl eu, ultricies efficitur lacus. ",
+  },
+  {
+    id: 4,
+    name: "Gorra",
+    price: 130.0,
+    category: "Accesorio",
+    description:
+      "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In vitae blandit ex, interdum pretium lacus. Cras tincidunt tortor a quam lobortis, in fringilla ipsum volutpat. Nullam elementum consequat lorem non hendrerit. Curabitur porttitor accumsan ex.",
+  },
+  {
+    id: 5,
+    name: "Bufanda",
+    price: 42.0,
+    category: "Accesorio",
+    description:
+      "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In vitae blandit ex, interdum pretium lacus. Cras tincidunt tortor a quam lobortis, in fringilla ipsum volutpat. Nullam elementum consequat lorem non hendrerit. Curabitur porttitor accumsan ex.",
+  },
+  {
+    id: 6,
+    name: "Ojotas",
+    price: 224.0,
+    category: "Footwear",
+    description:
+      "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In vitae blandit ex, interdum pretium lacus. Cras tincidunt tortor a quam lobortis, in fringilla ipsum volutpat. Nullam elementum consequat lorem non hendrerit. Curabitur porttitor accumsan ex.",
+  },
+  {
+    id: 7,
+    name: "Jeans",
+    price: 300.0,
+    category: "Pantalones",
+    description:
+      "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In vitae blandit ex, interdum pretium lacus. Cras tincidunt tortor a quam lobortis, in fringilla ipsum volutpat. Nullam elementum consequat lorem non hendrerit. Curabitur porttitor accumsan ex.",
+  },
+];
+
+export default items;
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <div class="product-cards-container">
+      <ProductDescriptionDrawer
+        :product="product"
+        :active="active.product_drawer"
+        v-on:close-product-drawer="closeDrawer()"
+      />
+      <ProductSummaryCard
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+        v-on:view-product="viewProduct($event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import items from "../data/items.js";
+import ProductSummaryCard from "../components/products/ProductSummaryCard.vue";
+import ProductDescriptionDrawer from "../components/products/ProductDescriptionDrawer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ProductSummaryCard,
+    ProductDescriptionDrawer,
+  },
+  methods: {
+    viewProduct(product) {
+      this.product = product;
+      this.active.product_drawer = true;
+    },
+    closeDrawer() {
+      this.active.product_drawer = false;
+    },
+  },
+  data() {
+    return {
+      product: null,
+      items: items,
+      active: {
+        product_drawer: false,
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.product-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
